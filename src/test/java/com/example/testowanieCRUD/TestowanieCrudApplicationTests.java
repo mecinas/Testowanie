84cb3c5724ea71cd50c976d15454f47999f3cd94
jzgagacz <01142603@pw.edu.pl>
2020-10-20 19:51:06 +0200

    Added test for Student: creating and finding by name

--- src/test/java/com/example/testowanieCRUD/TestowanieCrudApplicationTests.java
+++ src/test/java/com/example/testowanieCRUD/TestowanieCrudApplicationTests.java
@@ -1,13 +1,30 @@
 package com.example.testowanieCRUD;
 
+import com.example.testowanieCRUD.entity.Student;
+import com.example.testowanieCRUD.repository.StudentRepository;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-@SpringBootTest
+import java.time.LocalDate;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class TestowanieCrudApplicationTests {
 
+	@Autowired
+	private StudentRepository studentRepository;
+
 	@Test
-	void contextLoads() {
+	public void testCreateStudent(){
+		Student student = studentRepository.save(new Student("Ala", LocalDate.of(1999, 4, 4)));
+		assert(student.getId() > 0);
 	}
 
+	@Test
+	public void testFindStudentByName(){
+		Student student = studentRepository.findByNameIgnoreCase("Damian").get(0);
+		assert(student.getName().compareTo("Damian") == 0);
+	}
 }

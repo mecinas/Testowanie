06880cda236f1e9c68aa85c50d7459a38070e5f4
Wydzials <szymonstefan1999@gmail.com>
2020-12-06 11:04:07 +0100

    Fix sort algorithms in macro tests

--- Algorithms_Comparison/src/main/java/SortAndSearch.java
+++ Algorithms_Comparison/src/main/java/SortAndSearch.java
@@ -21,7 +21,7 @@ public class SortAndSearch {
 //    }
 
     public static int[] bubbleSortAndSequentialSearch(int[] arr, int[] x) {
-        int[] arrSorted = new InsertSort().sort(arr);
+        int[] arrSorted = new BubbleSort().sort(arr);
         return Arrays.stream(x).map(xx -> new SequentialSearch(arrSorted).findElementIndex(xx)).toArray();
     }
 
@@ -31,17 +31,17 @@ public class SortAndSearch {
     }
 
     public static int[] javaDefaultSortAndSequentialSearch(int[] arr, int[] x) {
-        int[] arrSorted = new InsertSort().sort(arr);
+        int[] arrSorted = new JavaDefaultSort().sort(arr);
         return Arrays.stream(x).map(xx -> new SequentialSearch(arrSorted).findElementIndex(xx)).toArray();
     }
 
     public static int[] mergeSortAndSequentialSearch(int[] arr, int[] x) {
-        int[] arrSorted = new InsertSort().sort(arr);
+        int[] arrSorted = new MergeSort().sort(arr);
         return Arrays.stream(x).map(xx -> new SequentialSearch(arrSorted).findElementIndex(xx)).toArray();
     }
 
     public static int[] quickSortAndSequentialSearch(int[] arr, int[] x) {
-        int[] arrSorted = new InsertSort().sort(arr);
+        int[] arrSorted = new Qsort().sort(arr);
         return Arrays.stream(x).map(xx -> new SequentialSearch(arrSorted).findElementIndex(xx)).toArray();
     }
 
